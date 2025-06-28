2379d5e37f583ed68e52c3f020c6a7312cba7ea6
Void <152130497+111void@users.noreply.github.com>
2025-06-29 03:44:33 +0700

    .

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+import apiRoutes from './routes/apiRoutes.js';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(cors());
+app.set("json spaces",2);
+app.use(express.json());
+
+app.use('/api', apiRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
+
+export default app;
